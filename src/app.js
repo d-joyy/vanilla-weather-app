15dcb66aa49b7797d3448dc6a71aa9ad610d73e9
Danielle <daniellejoyy91@gmail.com>
2021-11-14 17:14:14 -0500

    Project Completed

--- src/app.js
+++ src/app.js
@@ -75,7 +75,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c926c5d2706ac295b2b095de05fd3d1a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
